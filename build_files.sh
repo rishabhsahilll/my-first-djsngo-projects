c08b59a8416aa58234c62490eccc0ecc49cbc648
RISHABH SAHIL <164033987+rishabhsahilll@users.noreply.github.com>
2024-03-25 19:58:57 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,26 @@
-# Build the project
-echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+#!/bin/bash
 
-echo "Make Migration..."
+# Starting build process
+echo "BUILD START"
+
+# Installing dependencies
+python3.9 -m pip install -r requirements.txt 
+
+# Step 3: Update Dependencies
+python3.9 -m pip install --upgrade urllib3 cloudinary
+
+# Step 5: Check Dependencies
+python3.9 -m pip list | grep urllib3
+python3.9 -m pip list | grep cloudinary
+
+
+# Making migrations
+echo "Make Migration"
 python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.9 manage.py migrate --noinput 
 
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+# Collecting static files
+echo "Collect Static"
+python3.9 manage.py collectstatic --noinput 
+
+echo "Build process completed successfully"
