143bdcd8820d01cb4911de53d143843b4635b00f
RISHABH SAHIL <164033987+rishabhsahilll@users.noreply.github.com>
2024-03-25 18:21:00 +0530

    Update settings.py

--- EShop/settings.py
+++ EShop/settings.py
@@ -105,6 +105,7 @@ DATABASES = {
         }
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -139,14 +140,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Static Files Add
+STATIC_ROOT = BASE_DIR / "static-build" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-# Static Files Add
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static-build" / "static"
