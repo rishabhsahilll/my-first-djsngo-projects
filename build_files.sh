c09e634d44fe2d289f53fa09aea0304c570b86c8
RISHABH SAHIL <164033987+rishabhsahilll@users.noreply.github.com>
2024-03-25 17:27:47 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -5,7 +5,10 @@ python3.9 pip freeze
 python3.9 --version
 
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install Django
+python3.9 -m pip install psycopg2-binary
+python3.9 -m pip install django-environ
+python3.9 -m pip install cloudinary
 
 echo "Make Migration"
 python3.9 manage.py makemigrations --noinput
